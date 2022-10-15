4a761270954184cd35fa968084b6e0445a1a0c61
Hugo Fajardo <hugofajardo1@gmail.com>
2022-10-15 14:56:03 -0300

    Creacion de CharacterTypeController

---
+++ DapperCrudTutorial/Controllers/CharacterTypeController.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCrudTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterTypeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CharacterTypeController(IConfiguration config)
+        {
+            _config = config;
+        }
+        //METODO QUE DEVUELVE TODOS LOS CHARACTER
+        [HttpGet]
+        public async Task<ActionResult<List<CharacterType>>> GetAllCharacterTypes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<CharacterType> characterTypes = await SelectAllCharacterTypes(connection);
+            return Ok(characterTypes);
+        }
+
+
+
+        //METODO REFACTORIZADO QUE DEVUELVE TODOS LOS CHARACTER
+        private static async Task<IEnumerable<CharacterType>> SelectAllCharacterTypes(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM CharacterTypes";
+            var characterTypes = await connection.QueryAsync<CharacterType>(sql);
+
+            return characterTypes;
+        }
+    }
+}
